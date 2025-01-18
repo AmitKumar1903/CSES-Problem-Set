8e7e2430e279435fd1e415b99c0b2abbc96ef2d0
Amit Kumar <119384172+AmitKumar1903@users.noreply.github.com>
2025-01-18 13:56:39 +0530

    Update Permutations.java

--- src/Permutations.java
+++ src/Permutations.java
@@ -1,3 +1,40 @@
+/*
+ * Problem: Beautiful Permutation
+ * 
+ * A permutation of integers 1, 2, ..., n is called a "beautiful" permutation if there are no adjacent 
+ * elements whose difference is 1.
+ * 
+ * Given an integer n, construct a beautiful permutation if such a permutation exists.
+ * 
+ * Input:
+ * - The only input line contains an integer n (1 ≤ n ≤ 10^6).
+ * 
+ * Output:
+ * - Print a beautiful permutation of integers 1, 2, ..., n. 
+ * - If there are several solutions, you may print any of them. 
+ * - If there are no solutions, print "NO SOLUTION".
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * 5
+ * 
+ * Output:
+ * 2 4 1 3 5
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * 3
+ * 
+ * Output:
+ * NO SOLUTION
+ * 
+ * Notes:
+ * - For n = 1 or n = 2, there is no way to form a beautiful permutation, so the output should be "NO SOLUTION".
+ * - For other values of n, the solution involves printing even numbers first, followed by odd numbers to ensure the 
+ *   difference between adjacent elements is not 1.
+ */
 import java.util.*;
 public class Permutations{
     public static void main(String [] args){
